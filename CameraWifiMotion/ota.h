46de67024dab7787114782f465c38e382ac8166f
Alan <60433566+alanesq@users.noreply.github.com>
2021-03-14 10:08:03 +0000

    Update ota.h

--- CameraWifiMotion/ota.h
+++ CameraWifiMotion/ota.h
@@ -1,27 +1,33 @@
 /**************************************************************************************************
  *  
- *                                Over The Air updates (OTA) - 30Sep20
+ *      Over The Air updates (OTA) - 14Mar21
  * 
- *                                   part of the Webserver library
+ *      part of the BasicWebserver sketch - https://github.com/alanesq/BasicWebserver
  *                                   
- *                 In Arduino IDE Select "ESP32 dev module" not ESP32-cam, PSRAM enabled
+ *                                   
+ *      The /ota page is only available after a password has been supplied in the format:   http://x.x.x.x/?pwd=12345678                             
+ *                                   
+ *      If using an esp32cam module In Arduino IDE Select "ESP32 dev module" not "ESP32-cam" with PSRAM enabled
  *                     
- *                              access with  http://<esp ip address>/ota
  * 
  **************************************************************************************************
 
-    WARNING: This is not secure, anyone with access to your wifi can upload their own sketch !
+ 
+    To enable/disable OTA see setting at top of main sketch (#define ENABLE_OTA 1)
 
-    To enable/disable OTA see setting at top of main sketch (#define OTA_ENABLED 1)
+    Activate OTA with   http://<esp ip address>?pwd=12345678
+    Then access with    http://<esp ip address>/ota
 
  
  **************************************************************************************************/
 
 
-#include <Update.h>
+#if defined ESP32
+  #include <Update.h>
+#endif
+ 
 
-
-// forward declarations
+// forward declarations (i.e. details of all functions in this file)
   void otaSetup();
   void handleOTA();
 
@@ -34,69 +40,166 @@
 
 void otaSetup() {
 
-    server.on("/ota", handleOTA);
-   
-    server.on("/update", HTTP_POST, []() {
-      server.sendHeader("Connection", "close");
-      server.send(200, "text/plain", (Update.hasError()) ? "Update Failed!, rebooting" : "Update complete, device is rebooting...");
-      delay(500);
-      ESP.restart();
-      delay(2000);
-    }, []() {
-      HTTPUpload& upload = server.upload();
-      if (upload.status == UPLOAD_FILE_START) {
-        Serial.setDebugOutput(true);
-        Serial.printf("Update: %s\n", upload.filename.c_str());
-        if (!Update.begin()) { //start with max available size
-          Update.printError(Serial);
-        }
-      } else if (upload.status == UPLOAD_FILE_WRITE) {
-        if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-          Update.printError(Serial);
-        }
-      } else if (upload.status == UPLOAD_FILE_END) {
-        if (Update.end(true)) { //true to set the size to the current progress
-          Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-        } else {
-          Update.printError(Serial);
-        }
-        Serial.setDebugOutput(false);
-      } else {
-        Serial.printf("Update Failed Unexpectedly (likely broken connection): status=%d\n", upload.status);
-      }
-    });
+    OTAEnabled = 1;          // flag that OTA has been enabled
+
+    // server.on("/ota", handleOTA);
+
+    // esp32 version (using webserver.h)
+    #if defined ESP32
+        server.on("/update", HTTP_POST, []() {
+          server.sendHeader("Connection", "close");
+          server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+          ESP.restart();
+        }, []() {
+          HTTPUpload& upload = server.upload();
+          if (upload.status == UPLOAD_FILE_START) {
+            if (serialDebug) Serial.setDebugOutput(true);
+            if (serialDebug) Serial.printf("Update: %s\n", upload.filename.c_str());
+            if (!Update.begin()) { //start with max available size
+              if (serialDebug) Update.printError(Serial);
+            }
+          } else if (upload.status == UPLOAD_FILE_WRITE) {
+            if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+              if (serialDebug) Update.printError(Serial);
+            }
+          } else if (upload.status == UPLOAD_FILE_END) {
+            if (Update.end(true)) { //true to set the size to the current progress
+              if (serialDebug) Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+            } else {
+              if (serialDebug) Update.printError(Serial);
+            }
+            if (serialDebug) Serial.setDebugOutput(false);
+          } else {
+            if (serialDebug) Serial.printf("Update Failed Unexpectedly (likely broken connection): status=%d\n", upload.status);
+          }
+        });
+    #endif
+    
+    // esp8266 version  (using ESP8266WebServer.h)
+    #if defined ESP8266
+        server.on("/update", HTTP_POST, []() {
+          server.sendHeader("Connection", "close");
+          server.send(200, "text/plain", (Update.hasError()) ? "Update Failed!, rebooting" : "Update complete, device is rebooting...");
+          delay(500);
+          ESP.restart();
+          delay(2000);
+        }, []() {
+          HTTPUpload& upload = server.upload();
+          if (upload.status == UPLOAD_FILE_START) {
+            if (serialDebug) Serial.setDebugOutput(true);
+            WiFiUDP::stopAll();
+            if (serialDebug) Serial.printf("Update: %s\n", upload.filename.c_str());
+            uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+            if (!Update.begin(maxSketchSpace)) { //start with max available size
+              if (serialDebug) Update.printError(Serial);
+            }
+          } else if (upload.status == UPLOAD_FILE_WRITE) {
+            if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+              if (serialDebug) Update.printError(Serial);
+            }
+          } else if (upload.status == UPLOAD_FILE_END) {
+            if (Update.end(true)) { //true to set the size to the current progress
+              if (serialDebug) Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+            } else {
+              if (serialDebug) Update.printError(Serial);
+            }
+            if (serialDebug) Serial.setDebugOutput(false);
+          }
+          yield();
+        });
+    #endif
+
 }
 
 
-// ----------------------------------------------------------------
-//      -OTA web page requested     i.e. http://x.x.x.x/ota
-// ----------------------------------------------------------------
+// -----------------------------------------------------------------------
 
 void handleOTA(){
 
   WiFiClient client = server.client();          // open link with client
 
-  webheader(client);                            // add the standard html header
-
   // log page request including clients IP address
       IPAddress cip = client.remoteIP();
-      log_system_message("OTA web page requested from: " + String(cip[0]) +"." + String(cip[1]) + "." + String(cip[2]) + "." + String(cip[3]));
+      //log_system_message("OTA web page requested from: " + String(cip[0]) + "." + String(cip[1]) + "." + String(cip[2]) + "." + String(cip[3]));
 
-  client.write("<BR><H1>Update firmware</H1><BR>\n");
-  client.printf("Current version = %s, %s<BR><BR>", stitle, sversion);
+  // check if valid password supplied
+    if (server.hasArg("pwd")) {
+      if (server.arg("pwd") == OTAPassword) otaSetup();    // Enable over The Air updates (OTA)
+    }
+
+
+
+  // -----------------------------------------
+
+  if (OTAEnabled == 0) {
+    
+    // OTA is not enabled so request password to enable it
   
-  client.write("<form method='POST' action='/update' enctype='multipart/form-data'>\n");
-  client.write("<input type='file' style='width: 300px' name='update'>\n");
-  client.write("<BR><BR><input type='submit' value='Update'></form><BR>\n");
+      webheader(client);                            // add the standard html header
+  
+      client.print (R"=====(
+         <form name='loginForm'>
+            <table width='20%' bgcolor='A09F9F' align='center'>
+                <tr>
+                    <td colspan=2>
+                        <center><font size=4><b>Enter OTA password</b></font></center>
+                        <br>
+                    </td>
+                    <br>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><input type='Password' size=25 name='pwd'><br></td>
+                    <br>
+                    <br>
+                </tr>
+                <tr>
+                    <td><input type='submit' onclick='check(this.form)' value='Login'></td>
+                </tr>
+            </table>
+        </form>
+        <script>
+            function check(form)
+            {
+              window.open('/ota?pwd=' + form.pwd.value , '_self')
+            }
+        </script>
+      )=====");
 
-  client.write("<BR><BR>Device will reboot when upload complete");
-  client.printf("%s<BR>To disable OTA restart device<BR>%s", colRed, colEnd);
-                          
+      
+      webfooter(client);                          // add the standard web page footer
+
+  }
+
+  // -----------------------------------------
+
+  if (OTAEnabled == 1) {
+    
+    // Send page when OTA is enabled
+  
+      webheader(client);                            // add the standard html header
+    
+      client.write("<br><H1>Update firmware</H1><br>\n");
+      client.printf("Current version =  %s, %s \n\n", stitle, sversion);
+      
+      client.write("<form method='POST' action='/update' enctype='multipart/form-data'>\n");
+      client.write("<input type='file' style='width: 300px' name='update'>\n");
+      client.write("<br><br><input type='submit' value='Update'></form><br>\n");
+    
+      client.write("<br><br>Device will reboot when upload complete");
+      client.printf("%s <br>To disable OTA restart device<br> %s \n", colRed, colEnd);
+  
+      webfooter(client);                          // add the standard web page footer
+  }
+    
+  // -----------------------------------------
+
+                            
   // close html page
-    webfooter(client);                      // add the standard web page footer
     delay(3);
     client.stop();
-  
+    
 }
+
 
 // ---------------------------------------------- end ----------------------------------------------
